409786a7ba40890830ef8ab35c3729c3e03d21b9
DavidRaveloU <dravelo@unicesar.edu.co>
2025-02-17 20:02:11 -0500

    Implement game logic and UI for Wordspot game board

--- lib/widgets/wordspot_board.dart
+++ lib/widgets/wordspot_board.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:wordspot/constants/app_color.dart';
 import 'keyboard_word.dart';
 
 class WordspotBoard extends StatefulWidget {
@@ -13,7 +13,11 @@ class _WordspotBoardState extends State<WordspotBoard> {
   List<List<String>> board = List.generate(6, (_) => List.filled(5, ''));
   int currentRow = 0;
   int currentCol = 0;
-
+  final String wordOfTheDay = 'PERRO';
+  List<List<Color>> cellColors =
+      List.generate(6, (_) => List.filled(5, Colors.transparent));
+  List<List<Color>> borderColors =
+      List.generate(6, (_) => List.filled(5, Color(0XFF757575)));
   void onKeyPressed(String key) {
     setState(() {
       if (key == '⌫') {
@@ -23,51 +27,95 @@ class _WordspotBoardState extends State<WordspotBoard> {
         }
       } else if (key == '⏎') {
         if (currentCol == 5) {
-          currentRow++;
-          currentCol = 0;
+          String guessedWord = board[currentRow].join().toUpperCase();
+          if (guessedWord == wordOfTheDay) {
+            for (int i = 0; i < 5; i++) {
+              cellColors[currentRow][i] = Colors.green;
+            }
+            _showMessage('¡Felicidades! Has adivinado la palabra.');
+          } else {
+            _updateCellColors(guessedWord);
+            if (currentRow < 5) {
+              currentRow++;
+              currentCol = 0;
+            } else {
+              _showMessage('¡Oh no! La palabra era $wordOfTheDay.');
+            }
+          }
         }
       } else if (currentCol < 5) {
-        board[currentRow][currentCol] = key;
+        board[currentRow][currentCol] = key.toUpperCase();
         currentCol++;
       }
     });
   }
 
-  void updateCell(int row, int col, String letter) {
-    setState(() {
-      board[row][col] = letter;
-    });
+  void _updateCellColors(String guessedWord) {
+    for (int i = 0; i < 5; i++) {
+      if (guessedWord[i] == wordOfTheDay[i]) {
+        cellColors[currentRow][i] = Colors.green;
+        borderColors[currentRow][i] = Colors.green;
+      } else if (wordOfTheDay.contains(guessedWord[i])) {
+        cellColors[currentRow][i] = Colors.orange;
+        borderColors[currentRow][i] = Colors.orange;
+      } else {
+        cellColors[currentRow][i] = Color(0XFF757575);
+      }
+    }
+  }
+
+  void _showMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Expanded(
-        child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(6, (rowIndex) {
-                return Row(
+    return Scaffold(
+      backgroundColor: AppColor.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(5, (colIndex) {
-                    return Container(
-                      margin: EdgeInsets.all(4),
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                      ),
-                      child: Center(
-                        child: Text(board[rowIndex][colIndex]),
-                      ),
+                  children: List.generate(6, (rowIndex) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(5, (colIndex) {
+                        return Container(
+                          margin: const EdgeInsets.all(4),
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: borderColors[rowIndex][colIndex]),
+                            color: cellColors[rowIndex][colIndex],
+                          ),
+                          child: Center(
+                            child: Text(
+                              board[rowIndex][colIndex],
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
                     );
                   }),
-                );
-              })),
+                ),
+              ),
+            ),
+            KeyboardWord(onKeyPressed: onKeyPressed), // Teclado personalizado
+          ],
         ),
       ),
-      KeyboardWord(onKeyPressed: onKeyPressed),
-    ]));
+    );
   }
 }
